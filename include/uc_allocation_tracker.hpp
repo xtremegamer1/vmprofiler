ff1790ae22fe8acb77f60e1617115b8ba7257487
xtremegamer1 <rlyman308@gmail.com>
2022-10-14 07:13:44 -0600

    removed prints now that the memory leaks are gone

--- include/uc_allocation_tracker.hpp
+++ include/uc_allocation_tracker.hpp
@@ -4,3 +4,4 @@ extern int g_allocation_tracker;
 
 uc_err uct_context_alloc(uc_engine *uc, uc_context **context);
 uc_err uct_context_free(uc_context *context);
+void print_allocation_number();
--- src/uc_allocation_tracker.cpp
+++ src/uc_allocation_tracker.cpp
@@ -5,11 +5,18 @@ int g_allocation_tracker;
 
 uc_err uct_context_alloc(uc_engine *uc, uc_context **context)
 {
-  std::printf("Allocations: %p\n", ++g_allocation_tracker);
+  ++g_allocation_tracker;
+  //std::printf("Allocations: %p\n", g_allocation_tracker);
   return uc_context_alloc(uc, context);
 }
 uc_err uct_context_free(uc_context *context)
 {
-  std::printf("Allocations: %p\n", --g_allocation_tracker);
+  +g_allocation_tracker--;
+  //std::printf("Allocations: %p\n", g_allocation_tracker);
   return uc_context_free(context);
+}
+
+void print_allocation_number()
+{
+  std::printf("uc_context allocations: %p\n", g_allocation_tracker);
 }
